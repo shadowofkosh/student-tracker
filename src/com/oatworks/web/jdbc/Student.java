84166adb9f25eb49c6cb95f3e304688d8365acc2
shadowofkosh <43497467+shadowofkosh@users.noreply.github.com>
2018-09-25 14:54:26 +0200

    Update Student.java

--- src/com/oatworks/web/jdbc/Student.java
+++ src/com/oatworks/web/jdbc/Student.java
@@ -15,7 +15,6 @@ public class Student {
 		this.email = email;
 	}
 
-
 	public Student(int id, String firstName, String lastName, String email) {
 		super();
 		this.id = id;
@@ -24,62 +23,41 @@ public class Student {
 		this.email = email;
 	}
 
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-
 	public String getLastName() {
 		return lastName;
 	}
-
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}	
 
 }
